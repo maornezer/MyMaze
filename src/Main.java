4192210a08f30c41b1ee636b7286fb606f89b52b
maornezer <maorne@post.bgu.ac.il>
2024-11-03 20:58:35 +0200

    application title

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("View/MyView.fxml"));
         Parent root = fxmlLoader.load();
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Pacman Maze");
         Scene scene = new Scene(root, 700, 800);
         primaryStage.setScene(scene);
         primaryStage.show();
